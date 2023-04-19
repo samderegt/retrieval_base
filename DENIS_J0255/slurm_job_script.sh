fcdec80aed53cf6f457c6dd6444fb44fe05158b5
samderegt <regt@strw.leidenuniv.nl>
2023-04-19 11:07:35 +0200

    Fixed direction of barycentric correction

--- DENIS_J0255/slurm_job_script.sh
+++ DENIS_J0255/slurm_job_script.sh
@@ -3,12 +3,12 @@
 # Set job requirements
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --error=logs/%x_%j.err
-#SBATCH -t 01:00:00
+#SBATCH -t 00:25:00
 #SBATCH -p thin
-#SBATCH -n 100
+#SBATCH -n 110
 #SBATCH --mem=224G
 
-#SBATCH --job-name=DENIS_retrieval_142
+#SBATCH --job-name=DENIS_retrieval_143
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=regt@strw.leidenuniv.nl
 
