Fixed issue in reduced chi-squared calculation

retrieval_base/callback.py
@@ -7,6 +7,7 @@ import numpy as np
 import os
 import time
 import json
+import copy
 import corner
 
 import retrieval_base.auxiliary_functions as af
@@ -114,8 +115,12 @@ class CallBack:
             print(self.LogLike.beta.round(2))
         
         self.bestfit_params = np.array(self.bestfit_params)
-
+        
         if self.evaluation:
+            # Save the bestfit parameters in a .json file
+            # and the ModelSpectrum instance as .pkl
+            self.save_bestfit()
+            
             # Plot the CCFs + spectra of species' contributions
             figs.fig_species_contribution(
                 d_spec=self.d_spec, 
@@ -168,10 +173,6 @@ class CallBack:
         # Make a summary figure
         self.fig_summary()
 
-        # Save the bestfit parameters in a .json file
-        # and the ModelSpectrum instance as .pkl
-        self.save_bestfit()
-
         # Remove attributes from memory
         del self.Param, self.LogLike, self.PT, self.Chem, self.m_spec, self.pRT_atm, self.posterior
 
@@ -180,7 +181,7 @@ class CallBack:
 
     def save_bestfit(self):
         
-        # Save the bestfit parameters
+        # Save the best-fitting parameters
         params_to_save = {}
         for key_i, val_i in self.Param.params.items():
             if isinstance(val_i, np.ndarray):
@@ -198,8 +199,13 @@ class CallBack:
         with open(self.prefix+'data/bestfit.json', 'w') as fp:
             json.dump(dict_to_save, fp, indent=4)
 
-        # Save the bestfit spectrum
+        # Save the best-fitting spectrum
         af.pickle_save(self.prefix+'data/bestfit_m_spec.pkl', self.m_spec)
+
+        # Save the best-fitting log-likelihood
+        LogLike_to_save = copy.deepcopy(self.LogLike)
+        del LogLike_to_save.d_spec
+        af.pickle_save(self.prefix+'data/bestfit_LogLike.pkl', LogLike_to_save)
 
     def fig_abundances_corner(self):
 

retrieval_base/log_likelihood.py
@@ -85,14 +85,14 @@ class LogLikelihood:
 
                     # Scale the model flux to minimize the chi-squared error
                     m_flux_ij_scaled, f_ij = self.get_flux_scaling(d_flux_ij, m_flux_ij, Cov[i,j])
-                    res_ij_scaled = (d_flux_ij - m_flux_ij_scaled)
+                    res_ij = (d_flux_ij - m_flux_ij_scaled)
 
-                    # Chi-squared for the optimal linear scaling
-                    chi_squared_ij_scaled = np.dot(res_ij_scaled, Cov[i,j].solve(res_ij_scaled))
                 else:
-                    # Chi-squared without linear scaling of detectors
+                    # Without linear scaling of detectors
                     f_ij = 1
-                    chi_squared_ij_scaled = np.dot(res_ij, Cov[i,j].solve(res_ij))
+
+                # Chi-squared for the optimal linear scaling
+                chi_squared_ij_scaled = np.dot(res_ij, Cov[i,j].solve(res_ij))
                 
                 if self.scale_err:
                     # Scale the flux uncertainty that maximizes the log-likelihood