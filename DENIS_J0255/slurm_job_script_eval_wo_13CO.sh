5aab2f08bf4435d8aa543969af0f0f5f01c4d64f
samderegt <regt@strw.leidenuniv.nl>
2023-08-16 09:30:55 +0200

    Separated evaluation into different scripts

---
+++ DENIS_J0255/slurm_job_script_eval_wo_13CO.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Set job requirements
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --error=logs/%x_%j.err
+#SBATCH -t 10:30:00
+#SBATCH -p thin
+#SBATCH -n 16
+#SBATCH --mem=28G
+
+#SBATCH --job-name=eval_wo_13CO
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=regt@strw.leidenuniv.nl
+
+# Loading modules
+module load 2022
+module load Python/3.10.4-GCCcore-11.3.0
+module load OpenBLAS/0.3.20-GCC-11.3.0
+module load OpenMPI/4.1.4-GCC-11.3.0
+module load libarchive/3.6.1-GCCcore-11.3.0
+
+# Activate virtual environment
+source $HOME/retrieval_venv/bin/activate
+
+# Export environment variables
+export LD_LIBRARY_PATH=$HOME/retrieval_venv/MultiNest/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$HOME/retrieval_venv/SuiteSparse/lib:$LD_LIBRARY_PATH
+
+export pRT_input_data_path=$HOME/retrieval_venv/pRT_input_data
+
+echo "Number of tasks $SLURM_NTASKS"
+echo "Starting Python script"
+
+# Replace the config file and run evaluation
+cp retrieval.py retrieval_wo_13CO.py
+sed -i 's/import config_DENIS as conf/import config_DENIS_wo_13CO as conf/g' retrieval_wo_13CO.py
+
+echo "w/o 13CO"
+python retrieval_wo_13CO.py --evaluation
+
+rm -rf retrieval_wo_13CO.py
+
+echo "Done"
---
+++ DENIS_J0255/slurm_job_script_eval_wo_NH3.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Set job requirements
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --error=logs/%x_%j.err
+#SBATCH -t 10:30:00
+#SBATCH -p thin
+#SBATCH -n 16
+#SBATCH --mem=28G
+
+#SBATCH --job-name=eval_wo_NH3
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=regt@strw.leidenuniv.nl
+
+# Loading modules
+module load 2022
+module load Python/3.10.4-GCCcore-11.3.0
+module load OpenBLAS/0.3.20-GCC-11.3.0
+module load OpenMPI/4.1.4-GCC-11.3.0
+module load libarchive/3.6.1-GCCcore-11.3.0
+
+# Activate virtual environment
+source $HOME/retrieval_venv/bin/activate
+
+# Export environment variables
+export LD_LIBRARY_PATH=$HOME/retrieval_venv/MultiNest/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$HOME/retrieval_venv/SuiteSparse/lib:$LD_LIBRARY_PATH
+
+export pRT_input_data_path=$HOME/retrieval_venv/pRT_input_data
+
+echo "Number of tasks $SLURM_NTASKS"
+echo "Starting Python script"
+
+# Replace the config file and run evaluation
+cp retrieval.py retrieval_wo_NH3.py
+sed -i 's/import config_DENIS as conf/import config_DENIS_wo_NH3 as conf/g' retrieval_wo_NH3.py
+
+echo "w/o NH3"
+python retrieval_wo_NH3.py --evaluation
+
+rm -rf retrieval_wo_NH3.py
+
+echo "Done"
