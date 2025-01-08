2812a86c3b42ff561b2c5b1c7ed91d2ac2428e57
samderegt <regt@strw.leidenuniv.nl>
2025-01-08 16:30:48 +0100

    Update chemistry table with PH3+LiOH

--- Luhman_16/config_fiducial_J_A_1column.py
+++ Luhman_16/config_fiducial_J_A_1column.py
@@ -1,53 +1,55 @@
 import numpy as np
-import os
-
-file_params = 'config_fiducial_J_A_1column.py'
 
 ####################################################################################
 # Files and physical parameters
 ####################################################################################
 
-prefix = 'fiducial_J_A_ret_18_forced_cloud_n100'
+prefix = 'J_A_ret_19_1column_n1000'
 prefix = f'./retrieval_outputs/{prefix}/test_'
 
 config_data = dict(
-    J1226_A = {
-        'w_set': 'J1226', 'wave_range': (1115, 1338), 
-
-        'file_target': './data/Luhman_16A_J.dat', 
-        'file_std': './data/Luhman_16_std_J.dat', 
-        #'file_wave': './data/Luhman_16_std_J.dat', 
-        'file_wave': './data/Luhman_16_std_J_molecfit_transm.dat', # Use molecfit wlen-solution
-
-        # Telluric transmission
-        'file_molecfit_transm': './data/Luhman_16A_J_molecfit_transm.dat', 
-        'file_std_molecfit_transm': './data/Luhman_16_std_J_molecfit_transm.dat', 
-        'file_std_molecfit_continuum': './data/Luhman_16_std_J_molecfit_continuum.dat', 
-
-        'filter_2MASS': '2MASS/2MASS.J', # Magnitude used for flux-calibration
-        'pwv': 1.5, # Precipitable water vapour
-
-        # Telescope-pointing, used for barycentric velocity-correction
-        'ra': 162.299451, 'dec': -53.31767, 'mjd': 59946.35286502, 
-        'ra_std': 161.738984, 'dec_std': -56.75771, 'mjd_std': 59946.3601578, 
+    J1226_1 = dict(
+        instrument='CRIRES',
         
-        # Some info on standard-observation
-        'T_std': 15000, 
+        target_kwargs={
+            # Data filenames
+            'file':      './data/Luhman_16A_J_blend_corr.dat', 
+            'file_wave': './data/Luhman_16_std_J_molecfit_transm.dat', 
+            'file_molecfit_transm': './data/Luhman_16A_J_molecfit_transm.dat', 
 
-        # Slit-width, sets model resolution
-        'slit': 'w_0.4', 
+            # Mask pixels with lower telluric transmission
+            'telluric_threshold': 0.7, 
 
-        # Ignore pixels with lower telluric transmission
-        'tell_threshold': 0.7, 
-        'sigma_clip_width': 5, # Remove outliers
+            # Telescope-pointing, used for barycentric velocity-correction
+            'ra': 162.299451, 'dec': -53.31767, 'mjd': 59946.35286502, 
+
+            # Flux-calibration filter-name
+            'filter_name': '2MASS/2MASS.J', 'magnitude': 11.53, # Burgasser et al. (2013)
         }, 
-)
-#config_data['J1226_B'] = config_data['J1226_A'].copy()
 
-magnitudes = {
-    '2MASS/2MASS.J': (11.53, 0.04), # Burgasser et al. (2013)
-    '2MASS/2MASS.Ks': (9.44, 0.07), 
-}
+        std_kwargs={
+            # Data filenames
+            'file':      './data/Luhman_16_std_J.dat',
+            'file_wave': './data/Luhman_16_std_J_molecfit_transm.dat', 
+            'file_molecfit_transm':    './data/Luhman_16_std_J_molecfit_transm.dat', 
+            'file_molecfit_continuum': './data/Luhman_16_std_J_molecfit_continuum.dat',
+            'T_BB': 15000., # Blackbody temperature of the standard-star
+
+            # Telescope-pointing, used for barycentric velocity-correction
+            'ra': 161.738984, 'dec': -56.75771, 'mjd': 59946.3601578, 
+        }, 
+
+        kwargs={
+            # Observation info
+            'wave_range': (1115, 1338), 'w_set': 'J1226',  
+            #'wave_range': (1240, 1267), 'w_set': 'J1226',  
+            'slit': 'w_0.4', 'resolution': 65000,
+
+            # Outlier clipping
+            'sigma_clip_width': 5, 'sigma_clip_sigma': 5, 
+        },
+    ),
+)
 
 ####################################################################################
 # Model parameters
@@ -55,262 +57,171 @@ magnitudes = {
 
 # Define the priors of the parameters
 free_params = {
-
     # Covariance parameters
-    'log_a': [(-0.7,0.3), r'$\log\ a$'], 
-    'log_l': [(-3.0,-1.0), r'$\log\ l$'], 
+    'log_a': ['U', (-0.7,0.3), r'$\log\ a$'], 
+    'log_l': ['U', (-3.0,-1.0), r'$\log\ l$'], 
 
     # General properties
-    #'R_p': [(0.5,1.2), r'$R_\mathrm{p}$'], 
-    'log_g': [(3.5,6.0), r'$\log\ g$'], 
-    'epsilon_limb': [(0,1), r'$\epsilon_\mathrm{limb}$'], 
+    'M_p': ['G', (35.4,0.2), r'$\mathrm{M_p}$'], # (Bedin et al. 2024)
+    'R_p': ['G', (1.0,0.1), r'$\mathrm{R_p}$'], 
+    'rv':  ['U', (10.,30.), r'$v_\mathrm{rad}$'], 
 
-    # Velocities #km/s
-    'vsini': [(10.,30.), r'$v\ \sin\ i$'], 
-    'rv': [(10.,30.), r'$v_\mathrm{rad}$'], 
-
-    #'cloud_fraction': [(0.,1.), r'cf'], 
-    #'J1226_A': {
-    #    # Cloud properties
-    #    'log_opa_base_gray_0': [(-10,3), r'$\log\ \kappa_{\mathrm{cl,0,1}}$'], # Cloud slab
-    #    'log_P_base_gray_0': [(0.0,2.5), r'$\log\ P_{\mathrm{cl,0,1}}$'], 
-    #    'f_sed_gray_0': [(1,20), r'$f_\mathrm{sed,1}$'], 
-    #    'log_FeH': [(-14,-2), r'$\log\ \mathrm{FeH}$'], 
-    #    'log_FeH_P': [(-5,3), r'$\log\ P_\mathrm{FeH}$'], 
-    #}, 
-    #'J1226_B': {
-    #    # Cloud properties
-    #    'log_opa_base_gray_0': [(-10,3), r'$\log\ \kappa_{\mathrm{cl,0,1}}$'], # Cloud slab
-    #    'log_P_base_gray_0': [(0.0,2.5), r'$\log\ P_{\mathrm{cl,0,1}}$'], 
-    #    'f_sed_gray_0': [(1,20), r'$f_\mathrm{sed,1}$'], 
-    #    'log_FeH': [(-14,-2), r'$\log\ \mathrm{FeH}$'], 
-    #    'log_FeH_P': [(-5,3), r'$\log\ P_\mathrm{FeH}$'], 
-    #}, 
+    # Broadening
+    'vsini':        ['U', (10.,30.), r'$v\ \sin\ i$'], 
+    'epsilon_limb': ['U', (0,1), r'$\epsilon_\mathrm{limb}$'], 
 
     # Cloud properties
-    #'log_opa_base_gray_0': [(-10,3), r'$\log\ \kappa_{\mathrm{cl,0,1}}$'], # Cloud slab
-    #'log_P_base_gray_0': [(0.0,2.5), r'$\log\ P_{\mathrm{cl,0,1}}$'], 
-    #'f_sed_gray_0': [(1,20), r'$f_\mathrm{sed,1}$'], 
-    #'omega': [(0,1), r'$\omega$'], 
-
-    'log_FeH': [(-14,-2), r'$\log\ \mathrm{FeH}$'], 
-    'log_FeH_P': [(-5,3), r'$\log\ P_\mathrm{FeH}$'], 
-    'FeH_alpha': [(0,20), r'$\alpha_\mathrm{FeH}$'], 
+    'log_opa_base_gray': ['U', (-10,3), r'$\log\ \kappa_{\mathrm{cl,0}}$'], # Cloud slab
+    'log_P_base_gray':   ['U', (-0.5,2.5), r'$\log\ P_{\mathrm{cl,0}}$'], 
+    'f_sed_gray':        ['U', (1,20), r'$f_\mathrm{sed}$'], 
 
     # Chemistry
-    'log_H2O': [(-14,-2), r'$\log\ \mathrm{H_2O}$'], 
-    'log_HF': [(-14,-2), r'$\log\ \mathrm{HF}$'], 
-    'log_K': [(-14,-2), r'$\log\ \mathrm{K}$'], 
-    'log_Na': [(-14,-2), r'$\log\ \mathrm{Na}$'], 
+    'log_H2O':   ['U', (-14,-2), r'$\log\ \mathrm{H_2O}$'],
+    'log_HF':    ['U', (-14,-2), r'$\log\ \mathrm{HF}$'],
+    'log_K':     ['U', (-14,-2), r'$\log\ \mathrm{K}$'],
+    'log_Na':    ['U', (-14,-2), r'$\log\ \mathrm{Na}$'],
 
-    # Impact shifts: d = A*T^b * n/n_ref
-    # 0.0033 pm 0.0008 # 0.0036 pm 0.0006
-    #'A_d_0': [(0.0,0.007), r'$A_{d,0}$'], # 0.00158988 (K-H2) | 0.001943820 (K-He)
-    #'A_d_1': [(0.0,0.007), r'$A_{d,1}$'], # 0.00211668 (K-H2) | 0.000462539 (K-He)
+    'log_FeH':   ['U', (-14,-2), r'$\log\ \mathrm{FeH}$'], 
+    'log_FeH_P': ['U', (-5,3), r'$\log\ P_\mathrm{FeH}$'], 
+    'FeH_alpha': ['U', (0,20), r'$\alpha_\mathrm{FeH}$'], 
 
-    # 0.93 pm 0.03 # 0.90 pm 0.03
-    #'b_d_0': [(0.5,1.5), r'$b_{d,0}$'],   # 0.949254 (K-H2) | 0.89691 (K-He)
-    #'b_d_1': [(0.5,1.5), r'$b_{d,1}$'],   # 0.933563 (K-H2) | 1.07284 (K-He)
+    # PT profile
+    'dlnT_dlnP_0': ['U', (0.10,0.34), r'$\nabla_0$'], 
+    'dlnT_dlnP_1': ['U', (0.10,0.34), r'$\nabla_1$'], 
+    'dlnT_dlnP_2': ['U', (0.05,0.34), r'$\nabla_2$'], 
+    'dlnT_dlnP_3': ['U', (0.,0.34), r'$\nabla_3$'], 
+    'dlnT_dlnP_4': ['U', (0.,0.34), r'$\nabla_4$'], 
 
-    # Impact widths: w = A*T^b * n/n_ref
-    # 0.38 pm 0.04 # 0.17 pm 0.04
-    #'A_w_0': [(0.05,0.60), r'$A_{w,0}$'], # 0.352609 (K-H2) | 0.208190 (K-He)
-    #'A_w_1': [(0.00,0.45), r'$A_{w,1}$'], # 0.245926 (K-H2) | 0.121448 (K-He)
-
-    # 0.39 pm 0.02 # 0.50 pm 0.03
-    #'b_w_0': [(0.30,0.60), r'$b_{w,0}$'], # 0.385961 (K-H2) | 0.452833 (K-He)
-    #'b_w_1': [(0.30,0.65), r'$b_{w,1}$'], # 0.447971 (K-H2) | 0.531718 (K-He)
-
-    # PT profile    
-    'dlnT_dlnP_0': [(0.10,0.34), r'$\nabla_0$'], 
-    'dlnT_dlnP_1': [(0.10,0.34), r'$\nabla_1$'], 
-    'dlnT_dlnP_2': [(0.05,0.34), r'$\nabla_2$'], 
-    'dlnT_dlnP_3': [(0.,0.34), r'$\nabla_3$'], 
-    'dlnT_dlnP_4': [(0.,0.34), r'$\nabla_4$'], 
-
-    'T_phot': [(1200.,2200.), r'$T_\mathrm{phot}$'], 
-    'log_P_phot': [(-1.,1.), r'$\log\ P_\mathrm{phot}$'], 
-    'd_log_P_phot+1': [(0.5,3.), r'$\log\ P_\mathrm{up}$'], 
-    'd_log_P_phot-1': [(0.5,2.), r'$\log\ P_\mathrm{low}$'], 
+    'T_phot':         ['U', (1200.,2200.), r'$T_\mathrm{phot}$'], 
+    'log_P_phot':     ['U', (-1.,1.), r'$\log\ P_\mathrm{phot}$'], 
+    'd_log_P_phot+1': ['U', (0.5,3.), r'$\Delta P_\mathrm{+1}$'], 
+    'd_log_P_phot-1': ['U', (0.5,2.), r'$\Delta P_\mathrm{-1}$'], 
 }
 
 # Constants to use if prior is not given
 constant_params = {
-    # Define pRT's log-pressure grid
-    'log_P_range': (-5,3), 
-    'n_atm_layers': 50, 
-
-    # Down-sample opacities for faster radiative transfer
-    'lbl_opacity_sampling': 3, 
-
-    # Data resolution
-    'res': 65000, 
-
     # General properties
-    'parallax': 496,  # +/- 37 mas
-    'inclination': 26, # degrees
-
-    'do_scat_emis': True, 
+    #'parallax': 496,  # +/- 37 mas
+    'distance': 1.9960, # pc +/- 50 AU (Bedin et al. 2024)
 
     # Custom line opacity
     'A_w_0_H2': 0.352609, 'b_w_0_H2': 0.385961, 
     'A_w_1_H2': 0.245926, 'b_w_1_H2': 0.447971, 
     'A_d_0_H2': 0.00158988, 'b_d_0_H2': 0.949254, 
     'A_d_1_H2': 0.00211668, 'b_d_1_H2': 0.933563, 
-    
+
     'A_w_0_He': 0.208190, 'b_w_0_He': 0.452833, 
     'A_w_1_He': 0.121448, 'b_w_1_He': 0.531718, 
     'A_d_0_He': 0.001943820, 'b_d_0_He': 0.89691, 
     'A_d_1_He': 0.000462539, 'b_d_1_He': 1.07284, 
-
-    'log_opa_base_gray_0': 0.98, 
-    'log_P_base_gray_0': 1.46, 
-    'f_sed_gray_0': 8.36, 
-    'omega': 0.43, 
 }
 
-#'''
-#parent_dir = '/net/lem/data1/regt/retrieval_base/retrieval_base/custom_opacity_data/'
-parent_dir = '/home/sdregt/retrieval_base/retrieval_base/custom_opacity_data/'
-#old_result_dir = '/net/lem/data1/regt/retrieval_base/Luhman_16/retrieval_outputs/fiducial_J_B_ret_45_1column/'
-line_opacity_kwargs = [
-    {
-    # --- Potassium (K I) --------------------
-    ###
-    #'exists_in_pRT_atm': f'{old_result_dir}test_data/pRT_atm_J1226_A.pkl', 
-    ###
-
-    'NIST_states_file': f'{parent_dir}/K_I_states.txt', 
-    #'VALD_trans_file': f'{parent_dir}/K_I_transitions.txt', 
-    'VALD_trans_file': f'{parent_dir}/K_I_transitions_Kurucz.txt', 
-    'pRT_name': 'K_wo_J_doublets', 
-    'mass': 39.0983, 
-    'is_alkali': True, 
-    'E_ion': 35009.8140, #Z=0, # Potassium (K I)
-    
-    #'line_cutoff': 200, 
-    'line_cutoff': 1000, 
-    #n_density_ref=1e20, 
-    'log_gf_threshold': -2.0, 
-    'log_gf_threshold_exact': -0.5, 
-    #'pre_compute': True, 
-    'pre_compute': False, 
-
-    #'nu_0': [7983.67489, 8041.38112], 
-    #'log_gf': [-0.139, -0.439], 
-    #'E_low': [13042.876, 12985.170], 
-    #'gamma_N': [7.790, 7.790], 
-    #'gamma_vdW': [-7.021, -7.022], 
-    
-    'nu_0': [7983.655, 8041.365], 
-    'log_gf': [-0.063, -0.361], 
-    'E_low': [13042.896, 12985.186], 
-    'gamma_N': [7.83, 7.83], 
-    'gamma_vdW': [-7.46, -7.46], 
-    }, 
-    {
-    # --- Sodium (Na I) ----------------------
-    ###
-    #'exists_in_pRT_atm': f'{old_result_dir}test_data/pRT_atm_J1226_A.pkl', 
-    ###
-
-    'NIST_states_file': f'{parent_dir}/Na_I_states.txt', 
-    #'VALD_trans_file': f'{parent_dir}/Na_I_transitions.txt', 
-    'VALD_trans_file': f'{parent_dir}/Na_I_transitions_Kurucz.txt', 
-    'pRT_name': 'Na_wo_J_doublet', 
-    'mass': 22.989769, 
-    'is_alkali': True, 
-    'E_ion': 41449.451, #Z=0, # Sodium (Na I)
-    
-    #'line_cutoff': 200, 
-    'line_cutoff': 1000, 
-    #n_density_ref=1e20, 
-    'log_gf_threshold': -2.0, 
-    'log_gf_threshold_exact': -0.5, 
-    #'pre_compute': True, 
-    'pre_compute': False, 
-    }, 
-]
-#'''
 ####################################################################################
-#
-####################################################################################
-
-sum_m_spec = len(config_data) > 1
-
-scale_flux = True
-scale_err  = True
-apply_high_pass_filter = False
-
-cloud_kwargs = {
-    'cloud_mode': 'gray'
-    #'cloud_mode': 'EddySed', 'cloud_species': ['Mg2SiO4(c)_cd', 'MgSiO3(c)_cd', 'Fe(c)_cd'], 
-}
-
-#rotation_mode = 'integrate' # 'convolve'
-rotation_mode = 'convolve'
-
-####################################################################################
-# Chemistry parameters
-####################################################################################
-
-chem_kwargs = {
-    'chem_mode': 'free', #'SONORAchem' #'eqchem'
-
-    'line_species': [
-        'H2O_pokazatel_main_iso_Sam_new', 
-        'HF_main_iso_new', 
-        'FeH_main_iso_Sam', 
-        'K_wo_J_doublets', 
-        'Na_wo_J_doublet', 
-        #'K', 'Na', # On-the-fly treatment
-        #'K_static', 'Na_Sam'
-    ], 
-}
-
-species_to_plot_VMR = [
-    #'H2O', 'CH4', 'HF', 'FeH', 'Cr', 'Mn', 'AlH', 'SH', 'K', 'Na', 
-    'H2O', 'HF', 'FeH', 'K', 'Na', 
-    ]
-species_to_plot_CCF = species_to_plot_VMR
-
-####################################################################################
-# Covariance parameters
-####################################################################################
-
-cov_kwargs = dict(
-    cov_mode = 'GP', #None, 
-    
-    trunc_dist   = 3, 
-    scale_GP_amp = True, 
-
-    # Prepare the wavelength separation and
-    # average squared error arrays and keep 
-    # in memory
-    prepare_for_covariance = True, #False, 
-)
-
-if free_params.get('log_l') is not None:
-    cov_kwargs['max_separation'] = \
-        cov_kwargs['trunc_dist'] * 10**np.max(free_params['log_l'][0])
-
-####################################################################################
-# PT parameters
+# Physical model keyword-arguments
 ####################################################################################
 
 PT_kwargs = dict(
     PT_mode   = 'free_gradient', 
     n_T_knots = 5, 
-    PT_interp_mode = 'linear', 
-    symmetric_around_P_phot = False, 
+    interp_mode = 'linear', 
+
+    log_P_range = (-5.,3.), 
+    n_atm_layers = 50,
+)
+
+chem_kwargs = dict(
+    chem_mode = 'free', 
+    line_species = [
+        '1H2-16O__POKAZATEL', 
+        '1H-19F__Coxon-Hajig', 
+        '23Na__Kurucz', 
+        '39K__Kurucz',
+        '56Fe-1H__MoLLIST',
+    ], 
+)
+
+cloud_kwargs = dict(
+    cloud_mode = 'gray', 
+)
+
+rotation_kwargs = dict(
+    rotation_mode = 'convolve', 
+    inclination   = 26, # Degreees
+)
+
+custom_opacity_path = '/home/sdregt/retrieval_base/Luhman_16/custom_opacity_data/'
+#custom_opacity_path = '/net/lem/data1/regt/retrieval_base/Luhman_16/custom_opacity_data/'
+line_opacity_kwargs = {
+    '39K__Kurucz': {
+        'states_file': f'{custom_opacity_path}/K_I_states.txt',
+        'transitions_file': f'{custom_opacity_path}/K_I_transitions_Kurucz.txt', 
+
+        'mass': 39.0983, 'is_alkali': True, 'E_ion': 35009.8140, 
+        'custom_transitions': [{'nu_0': 7983.655}, {'nu_0': 8041.365}], 'n_ref': 1e20,
+        'line_cutoff': 1000, 'log_gf_cutoff': -2.5, 'log_gf_cutoff_exact': -0.5,
+    }, 
+    '23Na__Kurucz': {
+        'states_file': f'{custom_opacity_path}/Na_I_states.txt',
+        'transitions_file': f'{custom_opacity_path}/Na_I_transitions_Kurucz.txt', 
+
+        'mass': 22.989769, 'is_alkali': True, 'E_ion': 41449.451,
+        'line_cutoff': 1000, 'log_gf_cutoff': -2.5, 'log_gf_cutoff_exact': -0.5,
+    }
+}
+
+pRT_Radtrans_kwargs = dict(
+    #line_species               = chem_kwargs['line_species'],
+    line_species               = ['1H2-16O__POKAZATEL', '1H-19F__Coxon-Hajig', '56Fe-1H__MoLLIST'], 
+    rayleigh_species           = ['H2','He'],
+    gas_continuum_contributors = ['H2-H2','H2-He'],
+    cloud_species              = cloud_kwargs.get('cloud_species'), 
+    
+    line_opacity_mode             = 'lbl',
+    line_by_line_opacity_sampling = 3, # Faster radiative transfer by down-sampling
+    scattering_in_emission        = False, 
+    
+    pRT_input_data_path = '/projects/0/prjs1096/pRT3/input_data', 
+    #pRT_input_data_path = '/net/schenk/data2/regt/pRT3_input_data/input_data', 
+)
+
+####################################################################################
+# Log-likelihood, covariance keyword-arguments
+####################################################################################
+
+loglike_kwargs = dict(
+    scale_flux = True, scale_relative_to_chip = 19, 
+    scale_err = True, 
+    sum_model_settings = True, 
+)
+
+cov_kwargs = dict(
+    trunc_dist = 3, 
+    scale_amp  = True, 
+    max_wave_sep = 3 * 10**free_params.get('log_l', [None,[None,np.inf]])[1][1], 
+)
+
+all_model_kwargs = dict(
+    PT_kwargs=PT_kwargs, 
+    chem_kwargs=chem_kwargs, 
+    cloud_kwargs=cloud_kwargs, 
+    rotation_kwargs=rotation_kwargs,
+    line_opacity_kwargs=line_opacity_kwargs, 
+    pRT_Radtrans_kwargs=pRT_Radtrans_kwargs, 
+
+    cov_kwargs=cov_kwargs, 
+    loglike_kwargs=loglike_kwargs,
 )
 
 ####################################################################################
 # Multinest parameters
 ####################################################################################
 
-const_efficiency_mode = True
-sampling_efficiency = 0.05
-evidence_tolerance = 0.5
-n_live_points = 100
-n_iter_before_update = 200
+pymultinest_kwargs = dict(    
+    verbose = True, 
+
+    const_efficiency_mode = True, 
+    sampling_efficiency   = 0.05, 
+    evidence_tolerance    = 0.5, 
+    n_live_points         = 1000,
+    n_iter_before_update  = 200, 
+)
--- Luhman_16/config_fiducial_J_B_1column.py
+++ Luhman_16/config_fiducial_J_B_1column.py
@@ -62,7 +62,7 @@ free_params = {
     'log_l': ['U', (-3.0,-1.0), r'$\log\ l$'], 
 
     # General properties
-    'M_p': ['G', (29.4,0.2), r'$\mathrm{M_p}$'], 
+    'M_p': ['G', (29.4,0.2), r'$\mathrm{M_p}$'], # (Bedin et al. 2024)
     'R_p': ['G', (1.0,0.1), r'$\mathrm{R_p}$'], 
     'rv':  ['U', (10.,30.), r'$v_\mathrm{rad}$'], 
 
@@ -101,7 +101,8 @@ free_params = {
 # Constants to use if prior is not given
 constant_params = {
     # General properties
-    'parallax': 496,  # +/- 37 mas
+    #'parallax': 496,  # +/- 37 mas
+    'distance': 1.9960, # pc +/- 50 AU (Bedin et al. 2024)
 
     # Custom line opacity
     'A_w_0_H2': 0.352609, 'b_w_0_H2': 0.385961, 
@@ -148,8 +149,8 @@ rotation_kwargs = dict(
     inclination   = 26, # Degreees
 )
 
-#custom_opacity_path = '/home/sdregt/retrieval_base/retrieval_base/custom_opacity_data/'
-custom_opacity_path = '/net/lem/data1/regt/retrieval_base/Luhman_16/custom_opacity_data/'
+custom_opacity_path = '/home/sdregt/retrieval_base/Luhman_16/custom_opacity_data/'
+#custom_opacity_path = '/net/lem/data1/regt/retrieval_base/Luhman_16/custom_opacity_data/'
 line_opacity_kwargs = {
     '39K__Kurucz': {
         'states_file': f'{custom_opacity_path}/K_I_states.txt',
@@ -179,8 +180,8 @@ pRT_Radtrans_kwargs = dict(
     line_by_line_opacity_sampling = 3, # Faster radiative transfer by down-sampling
     scattering_in_emission        = False, 
     
-    #pRT_input_data_path = '/projects/0/prjs1096/pRT3/input_data', 
-    pRT_input_data_path = '/net/schenk/data2/regt/pRT3_input_data/input_data', 
+    pRT_input_data_path = '/projects/0/prjs1096/pRT3/input_data', 
+    #pRT_input_data_path = '/net/schenk/data2/regt/pRT3_input_data/input_data', 
 )
 
 ####################################################################################
